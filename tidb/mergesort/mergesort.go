97ee31defdefde2e54d095e24f2366d6b1239c56
XFFXFF <1247714429@qq.com>
2020-06-17 17:23:36 +0800

    make it better

--- tidb/mergesort/mergesort.go
+++ tidb/mergesort/mergesort.go
@@ -10,34 +10,35 @@ import (
 func MergeSort(src []int64) {
 	grLimit := runtime.NumCPU() - 1
 	grLimitChan := make(chan struct{}, grLimit)
-	mergeSort(src, grLimitChan)
+	temp := make([]int64, len(src))
+	mergeSort(src, temp, grLimitChan)
 }
 
-func mergeSort(src []int64, grLimitChan chan struct{}) {
+func mergeSort(src, temp []int64, grLimitChan chan struct{}) {
 	wg := sync.WaitGroup{}
 	length := len(src)
 	if length > 1 {
 		mid := length / 2
+		tempLeft := temp[:mid]
+		tempRight := temp[mid:]
 
 		select {
 		case grLimitChan <- struct{}{}:
 			wg.Add(1)
 			go func() {
 				defer wg.Done()
-				mergeSort(src[:mid], grLimitChan)
+				mergeSort(src[:mid], tempLeft, grLimitChan)
 			}()
 		default:
-			mergeSort(src[:mid], grLimitChan)
+			mergeSort(src[:mid], tempLeft, grLimitChan)
 		}
-		mergeSort(src[mid:], grLimitChan)
+		mergeSort(src[mid:], tempRight, grLimitChan)
 		wg.Wait()
-		merge(src, mid)
+		merge(src, tempLeft, tempRight, mid)
 	}
 }
 
-func merge(src []int64, mid int) {
-	leftSrc := make([]int64, mid)
-	rightSrc := make([]int64, len(src)-mid)
+func merge(src, leftSrc, rightSrc []int64, mid int) {
 	copy(leftSrc, src[:mid])
 	copy(rightSrc, src[mid:])
 
