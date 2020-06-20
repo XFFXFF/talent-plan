b96a3ae96ee4d7a128f3aa82cb81c44b727867b0
XFFXFF <1247714429@qq.com>
2020-06-20 08:50:01 +0800

    more faster

--- tidb/mapreduce/urltop10.go
+++ tidb/mapreduce/urltop10.go
@@ -1,9 +1,107 @@
 package main
 
-// URLTop10 .
+import (
+	"bytes"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+// URLTop10 generates RoundsArgs for getting the 10 most frequent URLs.
+// There are two rounds in this approach.
+// The first round will do url count.
+// The second will sort results generated in the first round and
+// get the 10 most frequent URLs.
 func URLTop10(nWorkers int) RoundsArgs {
-	// YOUR CODE HERE :)
-	// And don't forget to document your idea.
-	panic("YOUR CODE HERE")
-	return nil
+	var args RoundsArgs
+	// round 1: do url count
+	args = append(args, RoundArgs{
+		MapFunc:    URLCountMap,
+		ReduceFunc: URLCountReduce,
+		NReduce:    nWorkers,
+	})
+	// round 2: sort and get the 10 most frequent URLs
+	args = append(args, RoundArgs{
+		MapFunc:    URLTop10Map,
+		ReduceFunc: URLTop10Reduce,
+		NReduce:    1,
+	})
+	return args
+}
+
+// URLCountMap is the map function in the first round
+func URLCountMap(filename string, contents string) []KeyValue {
+	lines := strings.Split(contents, "\n")
+	kvs := make([]KeyValue, 0, len(lines))
+	for _, l := range lines {
+		l = strings.TrimSpace(l)
+		if len(l) == 0 {
+			continue
+		}
+		kvs = append(kvs, KeyValue{Key: l})
+	}
+	sort.Sort(KeySorter(kvs))
+
+	rets := []KeyValue{}
+	i := 0
+	for i < len(kvs) {
+		j := i + 1
+		for j < len(kvs) && kvs[j].Key == kvs[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, kvs[k].Value)
+		}
+		count := strconv.Itoa(len(values))
+		rets = append(rets, KeyValue{Key: kvs[i].Key, Value: count})
+		i = j
+	}
+
+	return rets
+}
+
+// URLCountReduce is the reduce function in the first round
+func URLCountReduce(key string, values []string) string {
+	count := 0
+	for _, value := range values {
+		valueInt, _ := strconv.Atoi(value)
+		count += valueInt
+	}
+	return fmt.Sprintf("%s %s\n", key, strconv.Itoa(count))
+}
+
+// URLTop10Map is the map function in the second round
+func URLTop10Map(filename string, contents string) []KeyValue {
+	lines := strings.Split(contents, "\n")
+	kvs := make([]KeyValue, 0, len(lines))
+	for _, l := range lines {
+		kvs = append(kvs, KeyValue{"", l})
+	}
+	return kvs
+}
+
+// URLTop10Reduce is the reduce function in the second round
+func URLTop10Reduce(key string, values []string) string {
+	cnts := make(map[string]int, len(values))
+	for _, v := range values {
+		v := strings.TrimSpace(v)
+		if len(v) == 0 {
+			continue
+		}
+		tmp := strings.Split(v, " ")
+		n, err := strconv.Atoi(tmp[1])
+		if err != nil {
+			panic(err)
+		}
+		cnts[tmp[0]] = n
+	}
+
+	us, cs := TopN(cnts, 10)
+	buf := new(bytes.Buffer)
+	for i := range us {
+		fmt.Fprintf(buf, "%s: %d\n", us[i], cs[i])
+	}
+	return buf.String()
 }
